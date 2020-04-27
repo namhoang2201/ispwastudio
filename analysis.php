9bf57c8e3f545a72636c9d7a9f44c6c1767874ee
namhoang2201 <hoangnamtb95@gmail.com>
2020-04-27 14:36:59 +0700

    fix bug

--- analysis.php
+++ analysis.php
@@ -15,146 +15,49 @@ if ($url) {
     curl_close($curl);
     if ($resp === FALSE) {
         // curl error
+        echo 'error';
     } else {
         // find all couple tag: <script></script> that include string "client"
-        preg_match('/<script+.+src="+.+\/client+.*><\/script>/', $resp, $matches);
+        preg_match_all('/<script[^>]*>(.*?)<\/script[^>]*>/', $resp, $matches);
         if (sizeof($matches)) {
-            foreach ($matches as $element) {
-                preg_match('/".*"/', $element, $matches_1);
-                if (sizeof($matches_1)) {
-                    foreach ($matches_1 as $itemUrl) {
-                        $itemUrl = str_replace('"', '', $itemUrl);
-                        if ($itemUrl[0] === '/') {
-                            $itemUrl = substr($itemUrl, 1);
-                        }
-                        if (strpos($itemUrl, "script") !== false) {
-                            // case: a number of adjacent script tags -> continue filter
-                            preg_match('/src=\/client+.+\.+js/', $itemUrl, $matches_2);
-                            if (sizeof($matches_2)) {
-                                foreach ($matches_2 as $subItemUrl) {
-
-                                    $subItemUrl = str_replace('src=', '', $subItemUrl);
-                                    if ($subItemUrl[0] === '/') {
-                                        $subItemUrl = substr($subItemUrl, 1);
-                                    }
-                                    $curl_2 = curl_init();
-                                    curl_setopt_array($curl_2, array(
-                                        CURLOPT_RETURNTRANSFER => 1,
-                                        CURLOPT_URL => $url . $subItemUrl,
-                                        CURLOPT_SSL_VERIFYPEER => false
-                                    ));
-                                    $resp_2 = curl_exec($curl_2);
-                                    curl_close($curl_2);
-                                    if ($resp_2 === FALSE) {
-                                        // curl error
-                                    } else {
-                                        if (strpos($resp_2, "M2_VENIA_BROWSER_PERSISTENCE") !== false || strpos($resp_2, "browserpersistence") !== false || strpos($resp_2, "NODE_ENV") !== false) {
-                                            $ispwaStudio = true;
-                                        }
-                                    }
+            foreach ($matches as $item) {
+                if (sizeof($item)) {
+                    foreach ($item as $subItem) {
+                        if (isset($subItem)) {
+                            preg_match('/client/', $subItem, $matches_1);
+                            if (sizeof($matches_1)) {
+                                $subItemUrl = preg_replace('/<script.*src="/', '', $subItem);
+                                $subItemUrl = preg_replace('/"><\/script>/', '', $subItemUrl);
+                                if ($subItemUrl[0] === '/') {
+                                    $subItemUrl = substr($subItemUrl, 1);
                                 }
-                            }
-                            if (!$ispwaStudio) {
-                                preg_match('/src=\/client+\.+js/', $itemUrl, $matches_3);
-                                if (sizeof($matches_3)) {
-                                    foreach ($matches_3 as $subItemUrl3) {
-                                        $subItemUrl3 = str_replace('src=', '', $subItemUrl3);
-                                        if ($subItemUrl3[0] === '/') {
-                                            $subItemUrl3 = substr($subItemUrl3, 1);
-                                        }
-                                        $curl_3 = curl_init();
-                                        curl_setopt_array($curl_3, array(
-                                            CURLOPT_RETURNTRANSFER => 1,
-                                            CURLOPT_URL => $url . $subItemUrl3,
-                                            CURLOPT_SSL_VERIFYPEER => false
-                                        ));
-                                        $resp_3 = curl_exec($curl_3);
-                                        curl_close($curl_3);
-                                        if ($resp_3 === FALSE) {
-                                            // curl error
-                                        } else {
-                                            if (strpos($resp_3, "M2_VENIA_BROWSER_PERSISTENCE") !== false || strpos($resp_3, "browserpersistence") !== false || strpos($resp_3, "NODE_ENV") !== false) {
-                                                $ispwaStudio = true;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-
-                            if (!$ispwaStudio) {
-                                preg_match('/src=+.[^\s]+\/client+.+\.+js/', $itemUrl, $matches_4);
-                                if (sizeof($matches_4)) {
-                                    foreach ($matches_4 as $subItemUrl4) {
-                                        $subItemUrl4 = str_replace('src=', '', $subItemUrl4);
-                                        if ($subItemUrl4[0] === '/') {
-                                            $subItemUrl4 = substr($subItemUrl4, 1);
-                                        }
-                                        $curl_4 = curl_init();
-                                        curl_setopt_array($curl_4, array(
-                                            CURLOPT_RETURNTRANSFER => 1,
-                                            CURLOPT_URL => $url . $subItemUrl4,
-                                            CURLOPT_SSL_VERIFYPEER => false
-                                        ));
-                                        $resp_4 = curl_exec($curl_4);
-                                        curl_close($curl_4);
-                                        if ($resp_4 === FALSE) {
-                                            // curl error
-                                        } else {
-                                            if (strpos($resp_4, "M2_VENIA_BROWSER_PERSISTENCE") !== false || strpos($resp_4, "browserpersistence") !== false || strpos($resp_4, "NODE_ENV") !== false) {
-                                                $ispwaStudio = true;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        } else {
-                            // case: all script tags are seperate
-                            $curl_5 = curl_init();
-                            curl_setopt_array($curl_5, array(
-                                CURLOPT_RETURNTRANSFER => 1,
-                                CURLOPT_URL => $url . $itemUrl,
-                                CURLOPT_SSL_VERIFYPEER => false
-                            ));
-                            $resp_5 = curl_exec($curl_5);
-                            curl_close($curl_5);
-                            if ($resp_5 === FALSE) {
-                                // curl error
-                            } else {
-                                if (strpos($resp_5, "M2_VENIA_BROWSER_PERSISTENCE") !== false || strpos($resp_5, "browserpersistence") !== false || strpos($resp_5, "NODE_ENV") !== false) {
-                                    $ispwaStudio = true;
-                                }
-                            }
-
-                            if (!$ispwaStudio) {
-                                // if url has some backslash
-                                if (substr_count($url, '/') > 2) {
-                                    if (strpos($url, 'https://') !== false) {
-                                        $sliceUrl = substr($url, 8);
-                                        $mainUrl = substr($sliceUrl, 0, 12);
-                                    }
-                                    $curl_6 = curl_init();
-                                    curl_setopt_array($curl_6, array(
-                                        CURLOPT_RETURNTRANSFER => 1,
-                                        CURLOPT_URL => 'https://' . $mainUrl . $itemUrl,
-                                        CURLOPT_SSL_VERIFYPEER => false
-                                    ));
-                                    $resp_6 = curl_exec($curl_6);
-                                    curl_close($curl_6);
-                                    if ($resp_6 === FALSE) {
-                                        // curl error
-                                    } else {
-                                        if (strpos($resp_6, "M2_VENIA_BROWSER_PERSISTENCE") !== false || strpos($resp_6, "browserpersistence") !== false || strpos($resp_6, "NODE_ENV") !== false) {
-                                            $ispwaStudio = true;
-                                        }
+                                $curl_1 = curl_init();
+                                curl_setopt_array($curl_1, array(
+                                    CURLOPT_RETURNTRANSFER => 1,
+                                    CURLOPT_URL => $url . $subItemUrl,
+                                    CURLOPT_SSL_VERIFYPEER => false
+                                ));
+                                $resp_1 = curl_exec($curl_1);
+                                curl_close($curl_1);
+                                if ($resp_1 === FALSE) {
+                                    // curl error
+                                } else {
+                                    if (strpos($resp_1, "M2_VENIA_BROWSER_PERSISTENCE") !== false || strpos($resp_1, "browserpersistence") !== false || strpos($resp_1, "NODE_ENV") !== false) {
+                                        $ispwaStudio = true;
+                                        break;
                                     }
                                 }
                             }
                         }
                     }
                 }
+                if ($ispwaStudio) {
+                    break;
+                }
             }
         } else {
             // curl error
+            echo 'error';
         }
     }
 }
@@ -164,4 +67,3 @@ if ($ispwaStudio) {
 } else {
     echo 'false';
 }
-?>
